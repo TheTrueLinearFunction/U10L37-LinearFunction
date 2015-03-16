e3383f8faa295ec8094f28153dba326c8dd32a91
Kevin Estrada <kipper609>
2015-03-16 11:18:59 -0700

    Revert "Committed changes"
    This reverts commit af19e271413b84b27c523ace31061b4182d64023.

--- LinearFunction.java
+++ LinearFunction.java
@@ -1,48 +1,11 @@
 
 /**
- * @author Kevin Estrada, Trevor Bych
- * @version 
+ * LinearFunction project codebase for Lesson 37
+ * 
+ * @author <Enter your name!>
+ * @version 2014-11-04
  */
 public class LinearFunction implements LinearFunctionMethods
 {
-    double m;
-    double b;
-    double root;
-    double x;
-    double y;
-    public LinearFunction (double m, double b)
-    {   
-        this.m = m;
-        this.b = b;
-                }
-    
-    public double getSlope()
-    {
-        return m;
-    }
-    
-    public double getYintercept()
-    {
-        return b;
-    }
-    
-    public double getRoot()
-    {
-        root = (-b)/m;
-        return root;
-    }
-    
-    public double getYvalue(double x)
-    {
-        this.x = x;
-        y = m*x +b;
-        return y;
-    }
-    
-    public double getXvalue(double y)
-    {
-        this.y = y;
-        x = (y-b)/m;
-        return x;
-    }    
+
 }
--- LinearFunctionMethods.java
+++ LinearFunctionMethods.java
@@ -9,39 +9,39 @@
  */
 public interface LinearFunctionMethods
 {
-    
-    /**
-     * Gets the slope of the linear function
-     * @returns The slope of the linear equation
-     */
-    public double getSlope();
+  /**
+   * Gets the slope of the linear function
+   * @returns The slope of the linear equation
+   */
+  public double getSlope();
+  
+  /**
+   * Gets the y-intercept of the linear function
+   * @returns The y-intercept of the linear equation
+   */
+  public double getYintercept();
 
-    /**
-     * Gets the y-intercept of the linear function
-     * @returns The y-intercept of the linear equation
-     */
-    public double getYintercept();
+  /**
+   * Gets the root of the linear equation.  The root is the 
+   * same as the "zero" or the x-coordinate where the line 
+   * crosses the x-axis.
+   * @returns The root of the linear equation
+   */
+  public double getRoot();
 
-    /**
-     * Gets the root of the linear equation.  The root is the 
-     * same as the "zero" or the x-coordinate where the line 
-     * crosses the x-axis.
-     * @returns The root of the linear equation
-     */
-    public double getRoot();
-
-    /**
-     * Given an x-value of a function, compute the y-value
-     * @param x Supplied x-value
-     * @returns The function's y-value for the supplied x-value
-     */
-    public double getYvalue(double x);
-
-    /**
-     * Given a y-value of a function, compute the x-value
-     * @param y Supplied y-value
-     * @returns The function's x-value for the supplied y-value
-     */
-    public double getXvalue(double y);
+  /**
+   * Given an x-value of a function, compute the y-value
+   * @param x Supplied x-value
+   * @returns The function's y-value for the supplied x-value
+   */
+  public double getYvalue(double x);
+  
+  /**
+   * Given a y-value of a function, compute the x-value
+   * @param y Supplied y-value
+   * @returns The function's x-value for the supplied y-value
+   */
+  public double getXvalue(double y);
 }
+    
 
